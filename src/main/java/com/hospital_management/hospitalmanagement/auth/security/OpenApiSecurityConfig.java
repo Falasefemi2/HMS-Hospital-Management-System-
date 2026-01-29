f4ce884fa863e7a848f425fd773854b037442c0f
Tomileenx <olarewajuoluwatomi@gmail.com>
2026-01-29 14:02:13 +0100

    Second Committ

---
+++ src/main/java/com/hospital_management/hospitalmanagement/auth/security/OpenApiSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.hospital_management.hospitalmanagement.auth.security;
+
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+
+
+@Configuration
+public class OpenApiSecurityConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
+    }
+}
