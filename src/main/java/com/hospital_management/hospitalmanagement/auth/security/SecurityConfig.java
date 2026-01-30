51cc41d94eb745505bd9e5fa854c3ac447e35467
Tomileenx <105252813+Tomileenx@users.noreply.github.com>
2026-01-30 22:00:09 +0100

    Import List in SecurityConfig
    Added import for List in SecurityConfig.java

--- src/main/java/com/hospital_management/hospitalmanagement/auth/security/SecurityConfig.java
+++ src/main/java/com/hospital_management/hospitalmanagement/auth/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.hospital_management.hospitalmanagement.auth.security;
 
+import java.util.List;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
