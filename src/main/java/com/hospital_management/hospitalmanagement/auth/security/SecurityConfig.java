b98ff1ca8073776412928a4c2b057fa632aab878
Tomileenx <105252813+Tomileenx@users.noreply.github.com>
2026-01-30 21:41:21 +0100

    Add CORS imports to SecurityConfig

--- src/main/java/com/hospital_management/hospitalmanagement/auth/security/SecurityConfig.java
+++ src/main/java/com/hospital_management/hospitalmanagement/auth/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.hospital_management.hospitalmanagement.auth.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,6 +12,9 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
